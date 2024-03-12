406b7730fe76ea145b0b826976b4e4dd32d2c7e1
Umair Shaik <umair.shaik@thoughtworks.com>
2024-03-12 11:24:08 +0530

    [Umair][Hooks][Step-2] - preparation for step-2 - create a template_home_page.dart

--- Flutter Hooks Example/flutter_hook_ex/lib/main.dart
+++ Flutter Hooks Example/flutter_hook_ex/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_hook_ex/step-1/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,32 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Step1HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
-class MyHomePage extends HookWidget {
-  final String title;
-
-  const MyHomePage({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    final dateTime = useStream(getTimeStream());
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(dateTime.data ?? 'data'),
-      ),
-    );
-  }
-}
-
-Stream<String> getTimeStream() => Stream.periodic(
-      const Duration(seconds: 1),
-      (_) => DateTime.now().toIso8601String(),
-    );
---
+++ Flutter Hooks Example/flutter_hook_ex/lib/step-1/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class Step1HomePage extends HookWidget {
+  final String title;
+
+  const Step1HomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    final dateTime = useStream(getTimeStream());
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(dateTime.data ?? 'data'),
+      ),
+    );
+  }
+}
+
+Stream<String> getTimeStream() => Stream.periodic(
+      const Duration(seconds: 1),
+      (_) => DateTime.now().toIso8601String(),
+    );
